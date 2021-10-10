d690a7e9df1ce9b69e1aa76c17f38b7504807100
rizkyyud <rizkyyud27@gmail.com>
2021-10-11 02:26:17 +0700

    Ngabab

---
+++ application/views/v_about.php
@@ -0,0 +1,295 @@
+
+<!DOCTYPE html>
+<html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>About Us</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<link rel="shorcut icon" type="text/css" href="<?php echo base_url().'assets/images/favicon.png'?>">
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/animate.css'?>">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/icomoon.css'?>">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/bootstrap.css'?>">
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/flexslider.css'?>">
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="<?php echo base_url().'theme/css/style.css'?>">
+
+	<!-- Modernizr JS -->
+	<script src="<?php echo base_url().'theme/js/modernizr-2.6.2.min.js'?>"></script>
+
+	</head>
+	<body>
+
+
+	<div id="fh5co-page">
+	<header id="fh5co-header" role="banner">
+		<div class="container">
+			<div class="header-inner">
+				<h1><a href="<?php echo base_url().''?>">Desa Ngabab</a></h1>
+				<nav role="navigation">
+					<ul>
+						<li><a href="<?php echo base_url().''?>">Home</a></li>
+						<li class="active"><a href="<?php echo base_url().'about'?>">About</a></li>
+						<li><a href="<?php echo base_url().'portfolio'?>">Portfolio</a></li>
+						<li><a href="<?php echo base_url().'artikel'?>">Blog</a></li>
+						<li><a href="<?php echo base_url().'gallery'?>">Gallery</a></li>
+						<li><a href="<?php echo base_url().'kontak'?>">Contact</a></li>
+						<li class="cta"><a href="<?php echo base_url().'portfolio'?>">Get started</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+
+
+	<aside id="fh5co-hero" clsas="js-fullheight">
+		<div class="flexslider js-fullheight">
+			<ul class="slides">
+		   	<li style="background-image: url('https://2.bp.blogspot.com/-P4JSOoOlfqg/V80vUsKaK5I/AAAAAAAAAKo/mFfB0ztjknIia6ZH6xfdbFo56XnDsayigCLcB/s1600/IMG-20160822-WA0001.jpg');">
+		   		<div class="overlay-gradient"></div>
+		   		<div class="container">
+		   			<div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
+		   				<div class="slider-text-inner">
+		   					<h2>Who We Are</h2>
+		   					<!-- <p class="fh5co-lead"> Awesome source code by <a href="http://mfikri.com/" target="_blank">M Fikri Setiadi</a></p> -->
+		   				</div>
+		   			</div>
+		   		</div>
+		   	</li>
+		  	</ul>
+	  	</div>
+	</aside>
+
+	<div class="fh5co-about animate-box">
+		<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+			<h2>About Us</h2>
+			<p align="justify">Desa Ngabab merupakan salah satu Desa yang berada di Kecamatan Pujon, Kabupaten Malang, Propinsi Jawa Timur. Dari segi topografi, Desa Ngabab berada di dataran tinggi dan berbukit, dengan ketinggian 1200 m di atas permukaan laut dengan kisaran suhu antara 8-18 °C, curah hujan 2,3 s/d 2400 mm/th dan terletak di sebelah Pasar Mantung, pusat transaksi sayur-mayur terbesar di wilayah Pujon. Desa Ngabab dilewati jalur utama lalu lintas darat yang menghubungkan antara kota Kediri dan kota Malang. Desa Ngabab berbatasan langsung dengan Desa Tulungrejo (sebelah barat), Desa Sukomulyo (sebelah selatan), Desa Tawang Sari (sebelah utara), dan Desa Ngroto (sebelah timur). Luas wilayah Desa Ngabab adalah 1244,93 Ha. yang digunakan sebagai hutan lindung, hutan produksi, ladang, pemukiman, perkebunan, dan fasilitas umum lainnya. Potensi yang paling menonjol dari Desa Ngabab adalah dari segi pertanian dan peternakan.</p>
+
+			<p align="justify">Desa Ngabab merupakan salah satu daerah paling potensial dalam segi pertanian di Kabupaten Malang. Ditinjau dari segi ekonomis, daerah ini merupakan salah satu penghasil sayur terbesar di Kabupaten Malang. Sayur mayur hasil pertanian dari daerah ini antara lain adalah wortel, cabe, kacang panjang, kentang, kubis, sawi, tomat, dan lainnya. Selain sayur mayur, potensi pertanian yang lain dari daerah ini adalah berupa tanaman buah-buahan (jeruk dan apel), tanaman palawija (jagung), dan tanaman padi (padi sawah). Pertanian dari daerah ini begitu subur karena didukung oleh potensi irigasi yang dimiliki, yaitu memiliki satu sungai dengan 12 mata air dan juga lahan pertanian yang sangat luas (787 Ha). Oleh karena itu, kualitas hasil pertanian dari daerah ini begitu tinggi dan terjamin. Hasil pertanian dari daerah Desa Ngabab telah tersebar ke berbagai daerah, seperti Jakarta, Kalimantan, Surabaya, Tuban, Lamongan, Jombang, Mojokerto, Blitar, dan lain sebagainya.</p>
+
+			<p align="justify">Selain dari segi pertanian, Desa Ngabab merupakan daerah yang sangat potensial dalam segi peternakan. Desa Ngabab adalah penghasil susu sapi terbesar yang ada di Kecamatan Pujon. Setiap hari, rata-rata produksi susu yang dihasilkan di Desa Ngabab adalah sebanyak 9000 liter (9 ton). Produksi susu sebanyak itu diperoleh dari 1900 ekor Sapi Perah yang dimiliki oleh para warga di Desa Ngabab. Hasil susu tersebut kemudian disetorkan ke KUD yang ada di Desa Ngabab setiap pagi dan sore, dan kemudian dipasok ke produsen susu bertaraf internasional, seperti Nestle dan Granfield. Kepada KUD, susu tersebut dijual Rp. 3000/liter. Jadi penghasilan dari Hasil Susu di Desa Ngabab adalah Rp. 27.000.000 untuk setiap harinya. Selain Sapi Perah, hewan ternak yang dimiliki oleh para warga di Desa Ngabab adalah Sapi Potong (12 ekor), Kambing (1500 ekor), Ayam Buras (3000 ekor), Itik (1000 ekor), Entok (200 ekor), dan Angsa (50 ekor).</p>
+
+			<p align="justify">Jumlah penduduk yang ada di Desa Ngabab adalah berjumlah 7412 orang dengan rincian 3677 orang berjenis kelamin perempuan dan 3735 orang berjenis kelamin laki-laki. Desa Ngabab terbagi menjadi 3 dusun, yaitu Dusun Krajan, Dusun Lemburejo, dan Dusun Torongrejo. Jumlah keseluruhan kepala keluarga yang ada di Desa Ngabab adalah 2185 KK. Struktur mata pencaharian penduduk Desa Ngabab adalah Petani (3087 orang), Pekerja sektor jasa/perdagangan (65 orang), dan pekerja sektor industri (41 orang).</p>
+		</div>
+		<div class="container">
+			<div class="col-md-6">
+				<figure>
+					<img src="https://desangabab.files.wordpress.com/2008/08/cimg00491.jpg" alt="Peta Desa Ngabab" class="img-responsive">
+				</figure>
+			</div>
+			<div class="col-md-6">
+				<h3>Visi</h3>
+				<ul>
+					<li>Menjadi Desa yang makmur, sejahtera, dan berwawasan, baik dalam kehidupan sosial maupun kehidupan keagamaan.</li>
+				</ul>
+				<h3>Misi</h3>
+				<ul>
+					<li>Menjadi Desa yang masyarakatnya selalu taat terhadap Agama yang dianut.</li>
+					<li>Selalu menjaga persatuan dan kesatuan antara warga yang satu dengan yang lainnya.</li>
+					<li>Membentuk kehidupan yang harmonis antara masyarakat yang ada di Desa Ngabab.</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="fh5co-team animate-box">
+		<div class="container">
+
+				<div class="row">
+					<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+						<h2>Perangkat Desa</h2>
+						<!-- <p>Kami memiliki tim yang solid. one team, one spirit, and one goal.</p> -->
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="https://desangabab.files.wordpress.com/2008/08/amin-afandi.jpg" alt="" class="img-responsive">
+						<h3>Amin Afandi</h3>
+						<h4>Kepala Desa</h4>
+						<!-- <p>M Fikri adalah seorang senior engineer lulusan perguruan tinggi di daerah Sumatera Barat.</p> -->
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="https://desangabab.files.wordpress.com/2008/08/hamam-royani.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Hamam Royani</h3>
+						<h4>Kaur Keuangan</h4>
+						<!-- <p>Dedi Irawan adalah seorang web designer lulusan perguruan tinggi di daerah Sumatera Barat.</p> -->
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="https://desangabab.files.wordpress.com/2008/08/samsul.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Samsul Arifin</h3>
+						<h4>Kebayan</h4>
+						<!-- <p>Afif Ma'ruf adalah seorang web designer lulusan perguruan tinggi di daerah Sumatera Barat.</p> -->
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="https://desangabab.files.wordpress.com/2008/08/m-choir.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>M. Choir</h3>
+						<h4>Kamituwo</h4>
+						<!-- <p>Afif Ma'ruf adalah seorang web designer lulusan perguruan tinggi di daerah Sumatera Barat.</p> -->
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="https://desangabab.files.wordpress.com/2008/08/nasrudin.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Nasruddin</h3>
+						<h4>Kesra 1</h4>
+						<!-- <p>Afif Ma'ruf adalah seorang web designer lulusan perguruan tinggi di daerah Sumatera Barat.</p> -->
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="https://desangabab.files.wordpress.com/2008/08/k-ajis.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Kiptirul Azis</h3>
+						<h4>Kesra 2</h4>
+						<!-- <p>Afif Ma'ruf adalah seorang web designer lulusan perguruan tinggi di daerah Sumatera Barat.</p> -->
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="https://desangabab.files.wordpress.com/2008/08/suparman.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Suparman</h3>
+						<h4>Kuwowo</h4>
+						<!-- <p>Afif Ma'ruf adalah seorang web designer lulusan perguruan tinggi di daerah Sumatera Barat.</p> -->
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="https://desangabab.files.wordpress.com/2008/08/arifin.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Arifin W.T.</h3>
+						<h4>Kepetengan</h4>
+						<!-- <p>Afif Ma'ruf adalah seorang web designer lulusan perguruan tinggi di daerah Sumatera Barat.</p> -->
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+					<div class="col-md-4 fh5co-staff">
+						<img src="https://desangabab.files.wordpress.com/2008/08/miskanudin.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Miskanudin</h3>
+						<h4>Kasun Lemburejo</h4>
+						<!-- <p>Afif Ma'ruf adalah seorang web designer lulusan perguruan tinggi di daerah Sumatera Barat.</p> -->
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div><div class="col-md-4 fh5co-staff">
+						<img src="https://desangabab.files.wordpress.com/2008/08/suliadi.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive">
+						<h3>Suliadi</h3>
+						<h4>Kasun Torong</h4>
+						<!-- <p>Afif Ma'ruf adalah seorang web designer lulusan perguruan tinggi di daerah Sumatera Barat.</p> -->
+						<ul class="fh5co-social">
+							<li><a href="#"><i class="icon-google"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+		</div>
+	</div>
+
+
+	<!-- <div class="fh5co-services">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading animate-box">
+					<h2>LAYANAN</h2>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/30.svg'?>" class="img-responsive"></span>
+					<h3>SOFTWARE & PROGRAM</h3>
+					<p>Tingkatkan kinerja perusahaan dengan Software yang sesuai dengan Business Process anda.</p>
+					<p><a href="<?php echo base_url().'portfolio'?>" class="btn btn-primary btn-outline with-arrow">Learn more <i class="icon-arrow-right"></i></a></p>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/18.svg'?>" class="img-responsive"></span>
+					<h3>IT CONSULTING</h3>
+					<p>Konsultasi kan kebutuhan IT anda pada kami dan ketahui layanan lain yang kami berikan.</p>
+					<p><a href="<?php echo base_url().'portfolio'?>" class="btn btn-primary btn-outline with-arrow">Learn more <i class="icon-arrow-right"></i></a></p>
+				</div>
+				<div class="col-md-4 text-center item-block">
+					<span class="icon"><img src="<?php echo base_url().'theme/images/27.svg'?>" class="img-responsive"></span>
+					<h3>WEB DESIGN</h3>
+					<p>Bangun identitas bisnis dan usaha anda di dunia Internet melalui Website.</p>
+					<p><a href="<?php echo base_url().'portfolio'?>" class="btn btn-primary btn-outline with-arrow">Learn more <i class="icon-arrow-right"></i></a></p>
+				</div>
+			</div>
+		</div>
+	</div> -->
+
+
+	<?php $this->load->view('v_footer');?>
+	</div>
+
+
+	<!-- jQuery -->
+	<script src="<?php echo base_url().'theme/js/jquery.min.js'?>"></script>
+	<!-- jQuery Easing -->
+	<script src="<?php echo base_url().'theme/js/jquery.easing.1.3.js'?>"></script>
+	<!-- Bootstrap -->
+	<script src="<?php echo base_url().'theme/js/bootstrap.min.js'?>"></script>
+	<!-- Waypoints -->
+	<script src="<?php echo base_url().'theme/js/jquery.waypoints.min.js'?>"></script>
+	<!-- Flexslider -->
+	<script src="<?php echo base_url().'theme/js/jquery.flexslider-min.js'?>"></script>
+
+	<!-- MAIN JS -->
+	<script src="<?php echo base_url().'theme/js/main.js'?>"></script>
+
+	</body>
+</html>
---
+++ application/views/v_footer.php
@@ -0,0 +1,44 @@
+<footer id="fh5co-footer" role="contentinfo">
+
+		<div class="container">
+			<div class="col-md-3 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>About Us</h3>
+				<p align="justify">Desa Ngabab merupakan salah satu Desa yang berada di Kecamatan Pujon, Kabupaten Malang, Propinsi Jawa Timur.</p>
+			</div>
+			<div class="col-md-3 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Potensi Desa</h3>
+				<ul class="float">
+					<li><a href="<?php echo base_url().'#'?>">Pendidikan</a></li>
+					<li><a href="<?php echo base_url().'#'?>">Pertanian</a></li>
+					<li><a href="<?php echo base_url().'#'?>">Peternakan</a></li>
+					<li><a href="<?php echo base_url().'#'?>">Bentang Alam</a></li>
+					<li><a href="<?php echo base_url().'#'?>">Sarana Wisata</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Fasilitas Umum</h3>
+				<ul class="float">
+					<li><a href="<?php echo base_url().'#'?>">Tempat Peribadatan</a></li>
+					<li><a href="<?php echo base_url().'#'?>">Prasarana Kesehatan</a></li>
+					<li><a href="<?php echo base_url().'#'?>">Prasarana Olahraga</a></li>
+				</ul>
+
+			</div>
+
+			<div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Follow Us</h3>
+				<ul class="fh5co-social">
+					<li><a href="#"><i class="icon-twitter"></i></a></li>
+					<li><a href="#"><i class="icon-facebook"></i></a></li>
+					<li><a href="#"><i class="icon-google-plus"></i></a></li>
+					<li><a href="#"><i class="icon-instagram"></i></a></li>
+				</ul>
+			</div>
+
+
+			<div class="col-md-12 fh5co-copyright text-center">
+				<p>&copy; 2018 by <a href="#" target="_blank">Team IT</a>. All Rights Reserved.</p>
+			</div>
+
+		</div>
+	</footer>
